7efe5f8c0d60549e4f7a63d131acc484250296e3
B.Larissa <109439888+ZigaLarissa@users.noreply.github.com>
2024-03-06 21:19:37 +0200

    authentication works

--- flutter_firebase_auth/lib/main.dart
+++ flutter_firebase_auth/lib/main.dart
@@ -1,6 +1,17 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_firebase_auth/screens/signup_screen.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: const FirebaseOptions(
+      apiKey: 'AIzaSyAYjjjtuH8WwnnVKDH6-IhbIhPFFLg0T2s',
+      appId: '1:290122446372:android:8cc13dfde88766b0d18ab1',
+      messagingSenderId: '290122446372',
+      projectId: 'flutter-firebase-auth-a5753',
+    ),
+  );
   runApp(const MainApp());
 }
 
@@ -10,11 +21,8 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+      debugShowCheckedModeBanner: false,
+      home: SignupPage(),
     );
   }
 }
--- flutter_firebase_auth/lib/screens/signup_screen.dart
+++ flutter_firebase_auth/lib/screens/signup_screen.dart
@@ -1,7 +1,47 @@
+// ignore_for_file: avoid_print
+
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
-class SignupPage extends StatelessWidget {
+final _auth = FirebaseAuth.instance;
+
+class SignupPage extends StatefulWidget {
   const SignupPage({super.key});
+
+  @override
+  State<SignupPage> createState() => _SignupPageState();
+}
+
+class _SignupPageState extends State<SignupPage> {
+  final _formKey = GlobalKey<FormState>();
+  bool isLogin = false;
+  String email = '';
+  String password = '';
+
+  Future<void> onValidate() async {
+    final isValid = _formKey.currentState!.validate();
+    if (!isValid) {
+      return;
+    }
+    _formKey.currentState!.save();
+    try {
+      if (isLogin) {
+        final existingUser = await _auth.signInWithEmailAndPassword(
+          email: email,
+          password: password,
+        );
+        print(existingUser);
+      } else {
+        final newUser = await _auth.createUserWithEmailAndPassword(
+          email: email,
+          password: password,
+        );
+        print(newUser);
+      }
+    } on FirebaseAuthException catch (err) {
+      print(err.message);
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -20,85 +60,93 @@ class SignupPage extends StatelessWidget {
                 Column(
                   children: <Widget>[
                     const SizedBox(height: 60.0),
-                    const Text(
-                      "Sign up",
-                      style: TextStyle(
+                    Text(
+                      isLogin ? "Login" : "Sign up",
+                      style: const TextStyle(
                         fontSize: 30,
                         fontWeight: FontWeight.bold,
+                        color: Colors.purple,
                       ),
                     ),
                     const SizedBox(
                       height: 20,
                     ),
                     Text(
-                      "Create your account",
+                      isLogin
+                          ? "Welcome back! Please login to your account"
+                          : "Create your account now",
                       style: TextStyle(fontSize: 15, color: Colors.grey[700]),
+                      textAlign: TextAlign.center,
                     )
                   ],
                 ),
-                Column(
-                  children: <Widget>[
-                    TextField(
-                      decoration: InputDecoration(
-                          hintText: "Username",
-                          border: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(18),
-                              borderSide: BorderSide.none),
-                          fillColor: Colors.purple.withOpacity(0.1),
-                          filled: true,
-                          prefixIcon: const Icon(Icons.person)),
-                    ),
-                    const SizedBox(height: 20),
-                    TextField(
-                      decoration: InputDecoration(
+                Form(
+                  key: _formKey,
+                  child: Column(
+                    children: <Widget>[
+                      const SizedBox(height: 20),
+                      TextFormField(
+                        decoration: InputDecoration(
                           hintText: "Email",
                           border: OutlineInputBorder(
                               borderRadius: BorderRadius.circular(18),
                               borderSide: BorderSide.none),
                           fillColor: Colors.purple.withOpacity(0.1),
                           filled: true,
-                          prefixIcon: const Icon(Icons.email)),
-                    ),
-                    const SizedBox(height: 20),
-                    TextField(
-                      decoration: InputDecoration(
-                        hintText: "Password",
-                        border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(18),
-                            borderSide: BorderSide.none),
-                        fillColor: Colors.purple.withOpacity(0.1),
-                        filled: true,
-                        prefixIcon: const Icon(Icons.password),
+                          prefixIcon: const Icon(Icons.email),
+                        ),
+                        validator: (value) {
+                          if (value == null ||
+                              value.isEmpty ||
+                              !value.contains('@')) {
+                            return 'Please enter a valid email address';
+                          }
+                          return null;
+                        },
+                        onSaved: (newValue) {
+                          email = newValue!;
+                        },
                       ),
-                      obscureText: true,
-                    ),
-                    const SizedBox(height: 20),
-                    TextField(
-                      decoration: InputDecoration(
-                        hintText: "Confirm Password",
-                        border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(18),
-                            borderSide: BorderSide.none),
-                        fillColor: Colors.purple.withOpacity(0.1),
-                        filled: true,
-                        prefixIcon: const Icon(Icons.password),
+                      const SizedBox(height: 20),
+                      TextFormField(
+                        decoration: InputDecoration(
+                          hintText: "Password",
+                          border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(18),
+                              borderSide: BorderSide.none),
+                          fillColor: Colors.purple.withOpacity(0.1),
+                          filled: true,
+                          prefixIcon: const Icon(Icons.password),
+                        ),
+                        obscureText: true,
+                        validator: (value) {
+                          if (value == null ||
+                              value.isEmpty ||
+                              value.trim().length < 7) {
+                            return 'A password must be at least 7 characters long';
+                          }
+                          return null;
+                        },
+                        onSaved: (newValue) {
+                          password = newValue!;
+                        },
                       ),
-                      obscureText: true,
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
                 Container(
                     padding: const EdgeInsets.only(top: 3, left: 3),
                     child: ElevatedButton(
-                      onPressed: () {},
-                      child: const Text(
-                        "Sign up",
-                        style: TextStyle(fontSize: 20),
-                      ),
+                      onPressed: onValidate,
                       style: ElevatedButton.styleFrom(
                         shape: const StadiumBorder(),
                         padding: const EdgeInsets.symmetric(vertical: 16),
                         backgroundColor: Colors.purple,
+                      ),
+                      child: Text(
+                        !isLogin ? "Sign up" : "Login",
+                        style:
+                            const TextStyle(fontSize: 20, color: Colors.white),
                       ),
                     )),
                 const Center(child: Text("Or")),
@@ -121,22 +169,11 @@ class SignupPage extends StatelessWidget {
                   ),
                   child: TextButton(
                     onPressed: () {},
-                    child: Row(
+                    child: const Row(
                       mainAxisAlignment: MainAxisAlignment.center,
                       children: [
-                        Container(
-                          height: 30.0,
-                          width: 30.0,
-                          decoration: const BoxDecoration(
-                            image: DecorationImage(
-                                image: AssetImage(
-                                    'assets/images/login_signup/google.png'),
-                                fit: BoxFit.cover),
-                            shape: BoxShape.circle,
-                          ),
-                        ),
-                        const SizedBox(width: 18),
-                        const Text(
+                        SizedBox(width: 18),
+                        Text(
                           "Sign In with Google",
                           style: TextStyle(
                             fontSize: 16,
@@ -150,12 +187,18 @@ class SignupPage extends StatelessWidget {
                 Row(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: <Widget>[
-                    const Text("Already have an account?"),
+                    Text(!isLogin
+                        ? "Already have an account?"
+                        : "Don't have an account?"),
                     TextButton(
-                        onPressed: () {},
-                        child: const Text(
-                          "Login",
-                          style: TextStyle(color: Colors.purple),
+                        onPressed: () {
+                          setState(() {
+                            isLogin = !isLogin;
+                          });
+                        },
+                        child: Text(
+                          !isLogin ? "Login" : "Sign up",
+                          style: const TextStyle(color: Colors.purple),
                         ))
                   ],
                 )
