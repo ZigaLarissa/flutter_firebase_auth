2cc6671f36b4ddb72f7814e12194beed40371aff
B.Larissa <109439888+ZigaLarissa@users.noreply.github.com>
2024-03-30 01:08:35 +0200

    Fixed the slight overflow on homescreen

--- flutter_firebase_auth/lib/screens/home_screen.dart
+++ flutter_firebase_auth/lib/screens/home_screen.dart
@@ -8,33 +8,35 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        padding: const EdgeInsets.only(left: 20, right: 20),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const SizedBox(height: 80),
-            const Text(
-              "Choose Category",
-              style: TextStyle(
-                fontSize: 24,
-                fontWeight: FontWeight.bold,
-                color: Color.fromARGB(255, 7, 7, 7),
+      body: Center(
+        child: Container(
+          padding: const EdgeInsets.only(left: 20, right: 20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const SizedBox(height: 80),
+              const Text(
+                "Choose Category",
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                  color: Color.fromARGB(255, 7, 7, 7),
+                ),
               ),
-            ),
-            const Text(
-              textAlign: TextAlign.left,
-              "Filter your goals by category to get a better view of your goals.",
-              style: TextStyle(
-                fontSize: 16,
-                color: Colors.black54,
+              const Text(
+                textAlign: TextAlign.left,
+                "Filter your goals by category to get a better view of your goals.",
+                style: TextStyle(
+                  fontSize: 16,
+                  color: Colors.black54,
+                ),
               ),
-            ),
-            SizedBox(
-              width: 400,
-              child: CategoryList(userId: userId),
-            ),
-          ],
+              SizedBox(
+                width: 400,
+                child: CategoryList(userId: userId),
+              ),
+            ],
+          ),
         ),
       ),
     );
